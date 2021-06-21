fc30ff06b606e0f36a0412af90ef59e789fba3e3
RBhor <ffest1@mail.ru>
2021-06-21 09:30:57 +0300

    Initial project version

---
+++ Lesson-Cpp/Test/revers.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX_A_SIZE = 100;
+
+int main()
+{
+    int N = MAX_A_SIZE;
+    int A[N];
+    int top = 0;
+    int x;
+
+    cin >> x;
+    while (x != 0){
+        A[top++]=x;
+
+        cin >> x;
+    }
+
+    for(int i = 0; i < top / 2; ++i) {
+        int tmp = A[i];
+        A[i] = A[top-1-i];
+        A[top-1-i] = tmp;
+    }
+
+    for(int i = 0; i < top; ++i) {
+        cout << A[i] << '\t';
+    }
+    cout << '\n';
+
+    return 0;
+}
